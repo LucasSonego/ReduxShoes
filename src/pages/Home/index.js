380649167a4b8cb19952c8a37617d425d7b90623
Lucas Sônego <sonego_lucas@outlook.com>
2020-02-25 16:10:35 -0300

    feat: homepage getting data from the api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { Container } from "./styles";
 
-import products from "../../products/products.js";
 import ProductCard from "../../components/ProductCard";
+import api from "../../services/api";
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function getApiData() {
+      const response = await api.get("products");
+      setProducts(response.data);
+    }
+    getApiData();
+  }, []);
+
   return (
     <Container>
       {products.map(p => (
