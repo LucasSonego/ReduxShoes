6137c13e5d099a035f4f9ffbcc9640efa16e7b9b
Lucas Sônego <lucassonego@ufpr.br>
2020-03-01 13:09:51 -0300

    feat: environment variales

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -2,7 +2,7 @@ import Reactotron from "reactotron-react-js";
 import { reactotronRedux } from "reactotron-redux";
 
 if (process.env.NODE_ENV === "development") {
-  const tron = Reactotron.configure()
+  const tron = Reactotron.configure({ host: process.env.REACT_APP_HOST })
     .use(reactotronRedux())
     .connect();
 
