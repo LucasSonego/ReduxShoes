309b6268ee3c682f4cf6035fef593aa4bbd36d5a
Lucas Sônego <sonego_lucas@outlook.com>
2020-03-01 13:09:51 -0300

    feat: environment variales

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import "dotenv/config";
 
 const api = axios.create({
-  baseURL: "http://192.168.0.102:3001",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
